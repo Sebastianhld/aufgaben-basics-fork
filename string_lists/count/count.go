f87783ff6fc9f944d7cad0696577d0c5958dccdd
sebastianhld <sebastian.jmheld@gmail.com>
2025-10-23 16:02:21 +0200

    fix

--- string_lists/count/count.go
+++
@@ -1,8 +0,0 @@
-package count
-
-// Erwartet eine Liste von Strings sowie einen String, der gezählt werden soll.
-// Liefer die Anzahl der Vorkommen des gesuchten Strings in der Liste.
-func Count(strings []string, search string) int {
-	// TODO
-	return 0
-}
